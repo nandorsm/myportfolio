3dc0153e37db843563944088aeb66c720386f9b0
Fernando <fernandocaetano933@gmail.com>
2022-10-20 14:13:59 -0300

    NavAtt

--- myportfolio/src/components/layout/Navbar.js
+++ myportfolio/src/components/layout/Navbar.js
@@ -1,17 +1,17 @@
 import { Link } from 'react-router-dom'
 
 import Container from './Container'
+import styles from './Navbar.module.css'
 
 function Navbar() {
     return(
-        <nav>
-            <Container>
+        <nav className={styles.navbar}>
+                <h2>MyPortfolio</h2>
                 <ul>
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/projects'>Projects</Link></li>
                     <li><Link to='/contact'>Contact</Link></li>
                 </ul>
-            </Container>
         </nav>
     )
 }
